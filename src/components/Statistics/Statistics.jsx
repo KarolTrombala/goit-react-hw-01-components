cbede299915b71702d56985d4d972aeb2f581ecb
Karol Trombala <ktrombala@gmail.com>
2023-09-25 22:54:05 +0200

    FriendList.jsx
    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -10,7 +10,7 @@ export const Statistics = ({ title, stats}) => {
 
     return (
         <section class="statistics">
-  <h2 class="title">{title}</h2>
+       <h2 class="title">{title}</h2>
 
   <ul class="stat-list">
     {stats.map(stat => (
